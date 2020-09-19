796765901b7aedbc1a33fcc85b568d9c9256ae92
David <dsi28@hotmail.com>
2020-09-19 18:12:37 -0500

    set up firebase anonymous auth

--- app/navigation/RootNavigation.tsx
+++ app/navigation/RootNavigation.tsx
@@ -1,14 +1,15 @@
 import {NavigationContainer, Theme} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {LoginScreen} from '../screens/auth/login/LoginScreen';
 import {ForgotPasswordScreen} from './../screens/auth/forgortPassword/ForgotPasswordScreen';
 import {RegisterScreen} from './../screens/auth/register/RegisterScreen';
-import {HomeScreen} from './../screens/Home/HomeScreen';
 import {Drawer, DrawerNav} from './drawer/DrawerNav';
 import {navigationRef} from './NavigationService';
 import {AuthParamList} from './types/AuthParamList';
 import {HomeParamList} from './types/HomeparamList';
+
+import auth from '@react-native-firebase/auth';
 
 interface RootNavigatorProps {
   theme: Theme;
@@ -18,6 +19,20 @@ const HomeStack = createStackNavigator<HomeParamList>();
 const AuthStack = createStackNavigator<AuthParamList>();
 
 export const RootNavigator: React.FC<RootNavigatorProps> = ({theme}) => {
+  useEffect(() => {
+    auth()
+      .signInAnonymously()
+      .then(() => {
+        console.log('User signed in anonymously');
+      })
+      .catch((error) => {
+        if (error.code === 'auth/operation-not-allowed') {
+          console.log('Enable anonymous in your firebase console.');
+        }
+        console.error(error);
+      });
+  }, []);
+
   const isLoggedIn = true;
   return (
     <NavigationContainer ref={navigationRef} theme={theme}>
