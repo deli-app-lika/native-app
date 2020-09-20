5b677a457271979be3d302f15636364129f8c7e1
David <dsi28@hotmail.com>
2020-09-20 14:25:18 -0500

    set check if user is signed in. if not set anonymous sigin

--- app/navigation/RootNavigation.tsx
+++ app/navigation/RootNavigation.tsx
@@ -4,22 +4,40 @@ import React, {useState, useEffect} from 'react';
 import {LoginScreen} from '../screens/auth/login/LoginScreen';
 import {ForgotPasswordScreen} from './../screens/auth/forgortPassword/ForgotPasswordScreen';
 import {RegisterScreen} from './../screens/auth/register/RegisterScreen';
-import {Drawer, DrawerNav} from './drawer/DrawerNav';
+import {DrawerNav} from './drawer/DrawerNav';
 import {navigationRef} from './NavigationService';
 import {AuthParamList} from './types/AuthParamList';
 import {HomeParamList} from './types/HomeparamList';
 
 import auth from '@react-native-firebase/auth';
+import {Button} from 'react-native';
 
 interface RootNavigatorProps {
   theme: Theme;
 }
 
-const HomeStack = createStackNavigator<HomeParamList>();
 const AuthStack = createStackNavigator<AuthParamList>();
 
 export const RootNavigator: React.FC<RootNavigatorProps> = ({theme}) => {
-  useEffect(() => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  // Handle user state changes
+  function onAuthStateChanged(user: any) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  //  sign out
+  const signUserOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+
+  // set ananymous auth
+  const anonymousSignIn = () => {
+    console.log('test anaymousignin');
     auth()
       .signInAnonymously()
       .then(() => {
@@ -31,15 +49,34 @@ export const RootNavigator: React.FC<RootNavigatorProps> = ({theme}) => {
         }
         console.error(error);
       });
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
   }, []);
 
-  const isLoggedIn = true;
+  if (initializing) {
+    console.log('inticalizing');
+    return null;
+  }
+
+  if (!user) {
+    console.log('user not signed in');
+    anonymousSignIn();
+  }
   return (
     <NavigationContainer ref={navigationRef} theme={theme}>
-      {isLoggedIn ? (
-        <DrawerNav />
-      ) : (
-        <AuthStack.Navigator
+      <DrawerNav />
+      {console.log(user)}
+      {/* button just to test ananymous sigin in after log out */}
+      <Button title="Sigin out" onPress={signUserOut} />
+    </NavigationContainer>
+  );
+};
+
+{
+  /* <AuthStack.Navigator
           screenOptions={{header: () => null}}
           initialRouteName="Login">
           <AuthStack.Screen name="Login" component={LoginScreen} />
@@ -48,8 +85,5 @@ export const RootNavigator: React.FC<RootNavigatorProps> = ({theme}) => {
             name="ForgotPassword"
             component={ForgotPasswordScreen}
           />
-        </AuthStack.Navigator>
-      )}
-    </NavigationContainer>
-  );
-};
+        </AuthStack.Navigator> */
+}
