6ad57fcf6f582945f2bc1b98714cda74ec247fd5
David <dsi28@hotmail.com>
2020-09-29 03:01:46 -0500

    add user long/lat to firestore. update user state when a user signs in(anonymously).

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,5 +1,11 @@
-interface IUser {
+export interface IUser {
+  isLoggedIn: boolean;
+  isNewUser: boolean;
   uid: string;
-  usernam;
+  isAnonymous: boolean;
+  location: {
+    long: number;
+    lad: number;
+  };
 }
 //everything except provider data and provider id.
--- app/navigation/drawer/DrawerNav.tsx
+++ app/navigation/drawer/DrawerNav.tsx
@@ -1,6 +1,6 @@
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createStackNavigator} from '@react-navigation/stack';
-import React, {ReactChild, ReactChildren} from 'react';
+import React, {ReactChild, ReactChildren, useEffect} from 'react';
 import {Button, View} from 'react-native';
 
 import {Account} from '../../screens/Account';
@@ -9,17 +9,96 @@ import {Home} from '../../screens/Home';
 import {HomeScreen} from '../../screens/Home/HomeScreen';
 import {BottomNav} from '../bottom/BottomNav';
 import {HomeParamList} from '../types/HomeparamList';
+import {PermissionsAndroid, PermissionStatus, Platform} from 'react-native';
+import Geolocation, {
+  GeolocationError,
+  GeolocationResponse,
+} from '@react-native-community/geolocation';
+import {
+  getCurPost,
+  getCurrentLocation,
+  getLocationPermission,
+} from '../../services/api/permissions/location';
+import {firestore} from 'firebase';
+import {useDispatch, useSelector} from 'react-redux';
+import {addAnonymousUser} from '../../services/api/firebase/users';
+import {setUserData} from '../../actions/auth';
 
 export const Drawer = createDrawerNavigator();
 const HomeStack = createStackNavigator<HomeParamList>();
 
 interface DrawerNavProps {}
 
-const renderHomeStack = () => (
-  <HomeStack.Navigator initialRouteName="Home">
-    <HomeStack.Screen name="Home" component={BottomNav} />
-  </HomeStack.Navigator>
-);
+const renderHomeStack = () => {
+  // @ts-ignore
+  const state = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const getLocationPermission = async () => {
+    // We need to ask permission for Android only
+    try {
+      if (Platform.OS === 'android') {
+        // Calling the permission function
+        console.log('plate form andorid');
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'LIKA would like to use your location',
+            message: 'Location message ',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        console.log(JSON.stringify('granted = ' + granted));
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('granted location permission');
+          Geolocation.getCurrentPosition(
+            (position: GeolocationResponse) => {
+              const initialPosition = JSON.stringify(position);
+              console.log('itial pos in get cur ' + initialPosition);
+              console.log('redux test ' + state.uid);
+              if (state.isLoggedIn) {
+                console.log('here we go(mario voice)');
+                const updatedUser = {
+                  ...state,
+                  ...{
+                    location: {
+                      long: position.coords.longitude,
+                      lad: position.coords.latitude,
+                    },
+                  },
+                };
+                addAnonymousUser(updatedUser);
+                dispatch(setUserData(updatedUser));
+              }
+            },
+            (error: GeolocationError) =>
+              console.log('Error', JSON.stringify(error)),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+          );
+        } else {
+          console.log('denied location permission');
+        }
+      } else {
+        // TODO IOS code
+        // getOneTimeLocation();
+        //subscribeLocationLocation();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getLocationPermission();
+  }, []);
+
+  return (
+    <HomeStack.Navigator initialRouteName="Home">
+      <HomeStack.Screen name="Home" component={BottomNav} />
+    </HomeStack.Navigator>
+  );
+};
 
 export const DrawerNav: React.FC<DrawerNavProps> = ({}) => {
   return (
