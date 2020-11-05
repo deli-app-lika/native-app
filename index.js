108a7275efac3d319f30dfc909f4a6175328a67c
David <dsi28@hotmail.com>
2020-11-04 23:37:08 -0600

    fix issue with after setting code of conduct

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import { EntryPoint } from './app/EntryPoint';
 import { name as appName } from './app.json';
+import EntryPoint from './app/EntryPoint';
 
 AppRegistry.registerComponent(appName, () => EntryPoint);
