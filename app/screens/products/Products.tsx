4aad82a4b748739181d0ac89ebde7768ab7c8e1a
David <dsi28@hotmail.com>
2020-11-21 03:59:45 -0600

    add heart icon to item

--- app/screens/products/Products.tsx
+++ app/screens/products/Products.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ScrollView, Text, View } from 'react-native';
 import FastImage from 'react-native-fast-image';
+import { Colors, IconButton } from 'react-native-paper';
 import styles from './styles';
 
 interface ProductsProps {}
@@ -306,6 +307,25 @@ const Products: React.FC<ProductsProps> = () => {
                       uri:
                         'https://cdn3.alegra.com/69f2f33437015bd18c9e459aae41f6a319953c9b-1598478470-WhatsApp+Image+2020-08-26+at+4.30.48+PM+%282%29.jpeg?Expires=1602973251&Signature=nwOsqp9wmZdYGH1vad5Pk~YGrzldPUrnRa~GrG2I8SQfg2zClzru4-CWyur8fHKhrFvEI23SR2D-rIslek7vbr5~vSkDI96oA5je~FHqJfuXxlMQguCU77mguCdENrK6bMthQ8N2nsqdeg1Xzp6wMq2YPx~tn-TKowP7ToedaS8K1s1Mmuy7KdkPKmPUHWUze8q9mBWBsRYnZ2v38C8MSqTFafNLzIauRRSxzwDaBGL51l-~AO7mtb6V1pEY9MyxN3UOpe6LOf7QMMh4FTbsoSRwrheWBLDwGX6YmCorlPIAuTP8NII~lMrJZbNWtSO3~MgF5z-wXXRewnydFnlOSA__&Key-Pair-Id=APKAJU3VE62QBWZP27QQ',
                       priority: FastImage.priority.normal
+                    }}
+                  />
+                  <IconButton
+                    style={{
+                      position: 'absolute',
+                      // left: 0,
+                      right: 0,
+                      top: 0,
+                      margin: 0,
+                      padding: 0
+                      // bottom: 0
+                      // backgroundColor: 'black'
+                    }}
+                    icon="heart-outline"
+                    color={Colors.orange800}
+                    size={25}
+                    onPress={() => {
+                      console.log('go to item');
+                      // NavigationService.goBack();
                     }}
                   />
                 </View>
