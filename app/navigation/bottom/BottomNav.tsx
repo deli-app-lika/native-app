4bbda36ed86966c6c5d82b5fb4586a0100932e06
ialikono <ialikono@microsoft.com>
2020-11-20 18:52:23 -0600

    imports

--- app/navigation/bottom/BottomNav.tsx
+++ app/navigation/bottom/BottomNav.tsx
@@ -1,11 +1,13 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+/* eslint-disable import/order */
+
 import Bartender from '../../screens/Bartender';
-import Favorites from '../../screens/favorites/Favorites';
-import Products from '../../screens/Products';
-import Search from '../../screens/Search';
 import { BottomParmList } from './BottomParmList';
+import Favorites from '../../screens/favorites/Favorites';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Products from '../../screens/Products';
+import React from 'react';
+import Search from '../../screens/Search/Search';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 interface BottomNavProps {}
 
