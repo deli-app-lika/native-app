25dfa716780690b7400e6933f7b3f958436bfbc1
David <dsi28@hotmail.com>
2020-11-21 18:40:18 -0600

    clean up items file. move inline styles out of file

--- app/screens/item/styles.ts
+++ app/screens/item/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { Colors } from 'react-native-paper';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,14 +10,48 @@ const styles = StyleSheet.create({
   },
   topHeader: {
     width: '100%',
-    // height: '4%',
-    height: 0,
+    height: '4%',
+    // height: 0,
     // marginBottom: 25,
     position: 'relative',
     // flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignSelf: 'flex-start'
-  }
+  },
+  topIcon: {
+    marginTop: 0,
+    paddingTop: 0
+  },
+  imageView: {
+    marginTop: 0,
+    paddingTop: 0
+    // height: '45%',
+    // backgroundColor: 'green'
+    // marginTop: 5
+  },
+  itemImage: {
+    marginTop: 0,
+    paddingTop: 0
+    // backgroundColor: 'yellow'
+  },
+  addToCart: {
+    position: 'absolute',
+    // left: 0,
+    right: 0,
+    bottom: 0,
+    // margin: 20,
+    // padding: 15,
+    // bottom: 0
+    backgroundColor: Colors.orange800
+  },
+  itemDetailsView: {
+    // backgroundColor: 'pink',
+    alignItems: 'flex-start',
+    flexDirection: 'column'
+  },
+  itemName: { fontWeight: 'bold', fontSize: 20 },
+  itemProperties: { fontSize: 15, fontWeight: 'bold' },
+  itemGroupProperties: { flexDirection: 'row' }
 });
 export default styles;
