c2f52f662236daf178c11b337f646b025c415436
David <dsi28@hotmail.com>
2020-11-21 18:23:14 -0600

    style item screen

--- app/screens/products/Products.tsx
+++ app/screens/products/Products.tsx
@@ -37,6 +37,8 @@ const Products: React.FC<ProductsProps> = ({ category }) => {
                     size: 750,
                     uid: '33eb45fb-9871-4eaa-bea3-28dad424833a',
                     unit: 'ML',
+                    image:
+                      'https://cdn3.alegra.com/69f2f33437015bd18c9e459aae41f6a319953c9b-1598478470-WhatsApp+Image+2020-08-26+at+4.30.48+PM+%282%29.jpeg?Expires=1602973251&Signature=nwOsqp9wmZdYGH1vad5Pk~YGrzldPUrnRa~GrG2I8SQfg2zClzru4-CWyur8fHKhrFvEI23SR2D-rIslek7vbr5~vSkDI96oA5je~FHqJfuXxlMQguCU77mguCdENrK6bMthQ8N2nsqdeg1Xzp6wMq2YPx~tn-TKowP7ToedaS8K1s1Mmuy7KdkPKmPUHWUze8q9mBWBsRYnZ2v38C8MSqTFafNLzIauRRSxzwDaBGL51l-~AO7mtb6V1pEY9MyxN3UOpe6LOf7QMMh4FTbsoSRwrheWBLDwGX6YmCorlPIAuTP8NII~lMrJZbNWtSO3~MgF5z-wXXRewnydFnlOSA__&Key-Pair-Id=APKAJU3VE62QBWZP27QQ',
                     updatedAt:
                       'Sat Nov 21 2020 17:20:34 GMT-0600 (Central Standard Time)'
                   });
