58c2847d016661d20c8aaf52f2b69d85bafd8160
David <dsi28@hotmail.com>
2020-11-26 14:38:16 -0600

    combine ingredient and inventory data to be displayed

--- app/screens/cocktail/Cocktail.tsx
+++ app/screens/cocktail/Cocktail.tsx
@@ -2,10 +2,18 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { Dimensions, ScrollView, View } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Colors, IconButton, Text } from 'react-native-paper';
+import {
+  ActivityIndicator,
+  Colors,
+  IconButton,
+  Text
+} from 'react-native-paper';
+import { ICocktail } from '../../models/cocktail';
 import NavigationService from '../../navigation/NavigationService';
 import { BartenderStackNavProps } from '../../navigation/types/BartenderParamList';
-import getIngredients from '../../services/api/ingredients/ingredients';
+import getIngredients, {
+  cleanResults
+} from '../../services/api/ingredients/ingredients';
 import styles from './styles';
 
 // interface CocktailProps {
@@ -22,24 +30,54 @@ const Cocktail: React.FC<BartenderStackNavProps<'Cocktail'>> = ({ route }) => {
     try {
       const temp = await getIngredients(params);
       console.log('temp', temp);
-      // setIngredientsList(await Promise.all(temp));
-      setIngredientsList(temp);
 
-      // Promise.all(temp).then((results) => {
-      //   setIngredientsList(results);
-      // });
+      const done = await Promise.all(temp).then((data) => {
+        // @ts-ignore
+        const doneData = cleanResults(data);
+        console.log('actually done', doneData);
+        return doneData;
+      });
+      console.log('done yoooooooo');
       // @ts-ignore
-
-      // // @ts-ignore
-      // getIngredients(params).then((returnedIngredients) => {
-      //   setIngredientsList(returnedIngredients);
-      // });
+      setIngredientsList(done);
     } catch (e) {
-      // TODO: Redirect to auth screen and logout user
-      console.log(e);
+      console.log('exception occured', e);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const renderIngredients = ingredientsList.map((cocktail: ICocktail) => {
+    console.log('cocktail===', cocktail);
+    return (
+      <TouchableOpacity
+        key={cocktail.uid}
+        onPress={() => {
+          NavigationService.navigate('Cocktail', cocktail);
+          // route.navigation.navigate('Cocktail');
+        }}
+      >
+        <View style={styles.ingredient}>
+          <IconButton
+            icon="circle-slice-8"
+            color={Colors.pink300}
+            size={20}
+            onPress={() => {}}
+          />
+          <FastImage
+            style={{ width: 25, height: 50 }}
+            source={{
+              uri:
+                'https://cdn3.alegra.com/69f2f33437015bd18c9e459aae41f6a319953c9b-1598478470-WhatsApp+Image+2020-08-26+at+4.30.48+PM+%282%29.jpeg?Expires=1602973251&Signature=nwOsqp9wmZdYGH1vad5Pk~YGrzldPUrnRa~GrG2I8SQfg2zClzru4-CWyur8fHKhrFvEI23SR2D-rIslek7vbr5~vSkDI96oA5je~FHqJfuXxlMQguCU77mguCdENrK6bMthQ8N2nsqdeg1Xzp6wMq2YPx~tn-TKowP7ToedaS8K1s1Mmuy7KdkPKmPUHWUze8q9mBWBsRYnZ2v38C8MSqTFafNLzIauRRSxzwDaBGL51l-~AO7mtb6V1pEY9MyxN3UOpe6LOf7QMMh4FTbsoSRwrheWBLDwGX6YmCorlPIAuTP8NII~lMrJZbNWtSO3~MgF5z-wXXRewnydFnlOSA__&Key-Pair-Id=APKAJU3VE62QBWZP27QQ',
+              priority: FastImage.priority.normal
+            }}
+            // resizeMode={FastImage.resizeMode.contain}
+          />
+          <Text style={styles.ingredientText}>LICOR IMPORTADO</Text>
+          <Text style={styles.ingredientPrice}>$50.00</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  });
 
   useEffect(() => {
     fetchInventory();
@@ -81,10 +119,10 @@ const Cocktail: React.FC<BartenderStackNavProps<'Cocktail'>> = ({ route }) => {
           source={{
             // TODO
             // @ts-ignore
-            uri: params.thumbnail,
+            uri: params.image,
             priority: FastImage.priority.normal
           }}
-          // resizeMode={FastImage.resizeMode.contain}
+          resizeMode={FastImage.resizeMode.contain}
         />
       </View>
       <View style={styles.titleCart}>
@@ -102,120 +140,14 @@ const Cocktail: React.FC<BartenderStackNavProps<'Cocktail'>> = ({ route }) => {
       </View>
       <View style={styles.ingredientsList}>
         <ScrollView>
-          <View style={styles.ingredient}>
-            <IconButton
-              icon="circle-slice-8"
-              color={Colors.pink300}
-              size={20}
-              onPress={() => {}}
-            />
-            <FastImage
-              style={{ width: 25, height: 50 }}
-              source={{
-                uri:
-                  'https://cdn3.alegra.com/69f2f33437015bd18c9e459aae41f6a319953c9b-1598478470-WhatsApp+Image+2020-08-26+at+4.30.48+PM+%282%29.jpeg?Expires=1602973251&Signature=nwOsqp9wmZdYGH1vad5Pk~YGrzldPUrnRa~GrG2I8SQfg2zClzru4-CWyur8fHKhrFvEI23SR2D-rIslek7vbr5~vSkDI96oA5je~FHqJfuXxlMQguCU77mguCdENrK6bMthQ8N2nsqdeg1Xzp6wMq2YPx~tn-TKowP7ToedaS8K1s1Mmuy7KdkPKmPUHWUze8q9mBWBsRYnZ2v38C8MSqTFafNLzIauRRSxzwDaBGL51l-~AO7mtb6V1pEY9MyxN3UOpe6LOf7QMMh4FTbsoSRwrheWBLDwGX6YmCorlPIAuTP8NII~lMrJZbNWtSO3~MgF5z-wXXRewnydFnlOSA__&Key-Pair-Id=APKAJU3VE62QBWZP27QQ',
-                priority: FastImage.priority.normal
-              }}
-              // resizeMode={FastImage.resizeMode.contain}
-            />
-            <Text style={styles.ingredientText}>LICOR IMPORTADO</Text>
-            <Text style={styles.ingredientPrice}>$50.00</Text>
-          </View>
-          <View style={styles.ingredient}>
-            <IconButton
-              icon="circle-slice-8"
-              color={Colors.pink300}
-              size={20}
-              onPress={() => {}}
-            />
-            <FastImage
-              style={{ width: 25, height: 50 }}
-              source={{
-                uri:
-                  'https://cdn3.alegra.com/69f2f33437015bd18c9e459aae41f6a319953c9b-1598478470-WhatsApp+Image+2020-08-26+at+4.30.48+PM+%282%29.jpeg?Expires=1602973251&Signature=nwOsqp9wmZdYGH1vad5Pk~YGrzldPUrnRa~GrG2I8SQfg2zClzru4-CWyur8fHKhrFvEI23SR2D-rIslek7vbr5~vSkDI96oA5je~FHqJfuXxlMQguCU77mguCdENrK6bMthQ8N2nsqdeg1Xzp6wMq2YPx~tn-TKowP7ToedaS8K1s1Mmuy7KdkPKmPUHWUze8q9mBWBsRYnZ2v38C8MSqTFafNLzIauRRSxzwDaBGL51l-~AO7mtb6V1pEY9MyxN3UOpe6LOf7QMMh4FTbsoSRwrheWBLDwGX6YmCorlPIAuTP8NII~lMrJZbNWtSO3~MgF5z-wXXRewnydFnlOSA__&Key-Pair-Id=APKAJU3VE62QBWZP27QQ',
-                priority: FastImage.priority.normal
-              }}
-              // resizeMode={FastImage.resizeMode.contain}
-            />
-            <Text style={styles.ingredientText}>LICOR IMPORTADO</Text>
-            <Text style={styles.ingredientPrice}>$50.00</Text>
-          </View>
-          <View style={styles.ingredient}>
-            <IconButton
-              icon="circle-slice-8"
-              color={Colors.pink300}
-              size={20}
-              onPress={() => {}}
-            />
-            <FastImage
-              style={{ width: 25, height: 50 }}
-              source={{
-                uri:
-                  'https://cdn3.alegra.com/69f2f33437015bd18c9e459aae41f6a319953c9b-1598478470-WhatsApp+Image+2020-08-26+at+4.30.48+PM+%282%29.jpeg?Expires=1602973251&Signature=nwOsqp9wmZdYGH1vad5Pk~YGrzldPUrnRa~GrG2I8SQfg2zClzru4-CWyur8fHKhrFvEI23SR2D-rIslek7vbr5~vSkDI96oA5je~FHqJfuXxlMQguCU77mguCdENrK6bMthQ8N2nsqdeg1Xzp6wMq2YPx~tn-TKowP7ToedaS8K1s1Mmuy7KdkPKmPUHWUze8q9mBWBsRYnZ2v38C8MSqTFafNLzIauRRSxzwDaBGL51l-~AO7mtb6V1pEY9MyxN3UOpe6LOf7QMMh4FTbsoSRwrheWBLDwGX6YmCorlPIAuTP8NII~lMrJZbNWtSO3~MgF5z-wXXRewnydFnlOSA__&Key-Pair-Id=APKAJU3VE62QBWZP27QQ',
-                priority: FastImage.priority.normal
-              }}
-              // resizeMode={FastImage.resizeMode.contain}
-            />
-            <Text style={styles.ingredientText}>LICOR IMPORTADO</Text>
-            <Text style={styles.ingredientPrice}>$50.00</Text>
-          </View>
-          <View style={styles.ingredient}>
-            <IconButton
-              icon="circle-slice-8"
-              color={Colors.pink300}
-              size={20}
-              onPress={() => {}}
-            />
-            <FastImage
-              style={{ width: 25, height: 50 }}
-              source={{
-                uri:
-                  'https://cdn3.alegra.com/69f2f33437015bd18c9e459aae41f6a319953c9b-1598478470-WhatsApp+Image+2020-08-26+at+4.30.48+PM+%282%29.jpeg?Expires=1602973251&Signature=nwOsqp9wmZdYGH1vad5Pk~YGrzldPUrnRa~GrG2I8SQfg2zClzru4-CWyur8fHKhrFvEI23SR2D-rIslek7vbr5~vSkDI96oA5je~FHqJfuXxlMQguCU77mguCdENrK6bMthQ8N2nsqdeg1Xzp6wMq2YPx~tn-TKowP7ToedaS8K1s1Mmuy7KdkPKmPUHWUze8q9mBWBsRYnZ2v38C8MSqTFafNLzIauRRSxzwDaBGL51l-~AO7mtb6V1pEY9MyxN3UOpe6LOf7QMMh4FTbsoSRwrheWBLDwGX6YmCorlPIAuTP8NII~lMrJZbNWtSO3~MgF5z-wXXRewnydFnlOSA__&Key-Pair-Id=APKAJU3VE62QBWZP27QQ',
-                priority: FastImage.priority.normal
-              }}
-              // resizeMode={FastImage.resizeMode.contain}
-            />
-            <Text style={styles.ingredientText}>LICOR IMPORTADO</Text>
-            <Text style={styles.ingredientPrice}>$50.00</Text>
-          </View>
-          <View style={styles.ingredient}>
-            <IconButton
-              icon="circle-slice-8"
-              color={Colors.pink300}
-              size={20}
-              onPress={() => {}}
-            />
-            <FastImage
-              style={{ width: 25, height: 50 }}
-              source={{
-                uri:
-                  'https://cdn3.alegra.com/69f2f33437015bd18c9e459aae41f6a319953c9b-1598478470-WhatsApp+Image+2020-08-26+at+4.30.48+PM+%282%29.jpeg?Expires=1602973251&Signature=nwOsqp9wmZdYGH1vad5Pk~YGrzldPUrnRa~GrG2I8SQfg2zClzru4-CWyur8fHKhrFvEI23SR2D-rIslek7vbr5~vSkDI96oA5je~FHqJfuXxlMQguCU77mguCdENrK6bMthQ8N2nsqdeg1Xzp6wMq2YPx~tn-TKowP7ToedaS8K1s1Mmuy7KdkPKmPUHWUze8q9mBWBsRYnZ2v38C8MSqTFafNLzIauRRSxzwDaBGL51l-~AO7mtb6V1pEY9MyxN3UOpe6LOf7QMMh4FTbsoSRwrheWBLDwGX6YmCorlPIAuTP8NII~lMrJZbNWtSO3~MgF5z-wXXRewnydFnlOSA__&Key-Pair-Id=APKAJU3VE62QBWZP27QQ',
-                priority: FastImage.priority.normal
-              }}
-              // resizeMode={FastImage.resizeMode.contain}
-            />
-            <Text style={styles.ingredientText}>LICOR IMPORTADO</Text>
-            <Text style={styles.ingredientPrice}>$50.00</Text>
-          </View>
-          <View style={styles.ingredient}>
-            <IconButton
-              icon="circle-slice-8"
-              color={Colors.pink300}
-              size={20}
-              onPress={() => {}}
-            />
-            <FastImage
-              style={{ width: 25, height: 50 }}
-              source={{
-                uri:
-                  'https://cdn3.alegra.com/69f2f33437015bd18c9e459aae41f6a319953c9b-1598478470-WhatsApp+Image+2020-08-26+at+4.30.48+PM+%282%29.jpeg?Expires=1602973251&Signature=nwOsqp9wmZdYGH1vad5Pk~YGrzldPUrnRa~GrG2I8SQfg2zClzru4-CWyur8fHKhrFvEI23SR2D-rIslek7vbr5~vSkDI96oA5je~FHqJfuXxlMQguCU77mguCdENrK6bMthQ8N2nsqdeg1Xzp6wMq2YPx~tn-TKowP7ToedaS8K1s1Mmuy7KdkPKmPUHWUze8q9mBWBsRYnZ2v38C8MSqTFafNLzIauRRSxzwDaBGL51l-~AO7mtb6V1pEY9MyxN3UOpe6LOf7QMMh4FTbsoSRwrheWBLDwGX6YmCorlPIAuTP8NII~lMrJZbNWtSO3~MgF5z-wXXRewnydFnlOSA__&Key-Pair-Id=APKAJU3VE62QBWZP27QQ',
-                priority: FastImage.priority.normal
-              }}
-              // resizeMode={FastImage.resizeMode.contain}
-            />
-            <Text style={styles.ingredientText}>LICOR IMPORTADO</Text>
-            <Text style={styles.ingredientPrice}>$50.00</Text>
-          </View>
+          {ingredientsList && ingredientsList.length > 1 ? (
+            renderIngredients
+          ) : (
+            <View style={styles.container}>
+              <Text>Loading...</Text>
+              <ActivityIndicator animating color={Colors.pink800} />
+            </View>
+          )}
         </ScrollView>
       </View>
       <View style={styles.directionText}>
