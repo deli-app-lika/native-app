67e2d7bb43c9c2ed80f6de39a70972a62e67605a
David <dsi28@hotmail.com>
2020-11-26 12:04:56 -0600

    resolving promises in cocktail file

--- app/services/api/ingredients/ingredients.ts
+++ app/services/api/ingredients/ingredients.ts
@@ -1,7 +1,45 @@
 import firestore from '@react-native-firebase/firestore';
 import { ICocktail } from '../../../models/cocktail';
 
-const getIngredients = async (cocktail: ICocktail) => {
+// const getIngredients1 = (cocktail: ICocktail) => {
+//   const { ingredients } = cocktail;
+//   console.log('ingreedients', ingredients);
+
+//   const inventoryRef = firestore()
+//     .collection('Shops')
+//     // TODO use geo location to get shop
+//     .doc('b36af212-d100-46ed-a758-71c7d2e1040c')
+//     .collection('Inventory');
+
+//   // const ingredientList =
+//   let i = 0;
+//   return ingredients.map(async (ingredient) => {
+//     i += 1;
+//     const item = await inventoryRef
+//       // TODO update this query to use name and not itemName
+//       .where('itemName', '==', ingredient.ingredient)
+//       .get();
+
+//     console.log('shoppppp', i, item);
+
+//     if (item.empty) {
+//       console.log('No matching documents.');
+//       // eslint-disable-next-line no-useless-return
+//       return;
+//     }
+//     return item;
+
+//     // item.forEach((doc) => {
+//     //   console.log('results loop', doc.id, '=>', doc.data());
+//     // });
+
+//     // eslint-disable-next-line consistent-return
+//     // return item;
+//   });
+//   // return ingredientList;
+// };
+
+const getIngredients = (cocktail: ICocktail) => {
   const { ingredients } = cocktail;
   console.log('ingreedients', ingredients);
 
@@ -11,23 +49,19 @@ const getIngredients = async (cocktail: ICocktail) => {
     .doc('b36af212-d100-46ed-a758-71c7d2e1040c')
     .collection('Inventory');
 
-  const ingredientList = ingredients.map(async (ingredient) => {
-    const item = await inventoryRef
+  const promises = ingredients.map(async (ingre) => {
+    // request details from GitHub’s API with Axios
+    const response = await inventoryRef
       // TODO update this query to use name and not itemName
-      .where('itemName', '==', ingredient.ingredient)
+      .where('itemName', '==', ingre.ingredient)
       .get();
 
-    console.log('shoppppp', item);
-    if (item.empty) {
-      console.log('No matching documents.');
-      return;
-    }
-
-    item.forEach((doc) => {
-      console.log('results loop', doc.id, '=>', doc.data());
-    });
+    return {
+      response
+    };
   });
-  return ingredientList;
+  // const results = await Promise.all(promises);
+  return promises;
 };
 
 export default getIngredients;
